a3b77aa1e12b2d72e8f0ca9051c7110b2bf77d9f
Michael Mitchell <michael@mitchtech.net>
2015-07-10 00:55:33 -0400

    Material design preferences from support library widgets

--- app/src/main/java/net/mitchtech/xposed/AllCapsNoCapsPreferenceActivity.java
+++ app/src/main/java/net/mitchtech/xposed/AllCapsNoCapsPreferenceActivity.java
@@ -3,42 +3,75 @@ package net.mitchtech.xposed;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
-import android.preference.SwitchPreference;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.cgollner.unclouded.preferences.SwitchPreferenceCompat;
+
 import net.mitchtech.xposed.allcapsnocaps.R;
 
-public class AllCapsNoCapsPreferenceActivity extends PreferenceActivity implements
-        OnSharedPreferenceChangeListener {
+public class AllCapsNoCapsPreferenceActivity extends AppCompatActivity {
 
     private static final String TAG = AllCapsNoCapsPreferenceActivity.class.getSimpleName();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getPreferenceManager().setSharedPreferencesMode(Context.MODE_WORLD_READABLE);
-        addPreferencesFromResource(R.xml.settings);
+//        getPreferenceManager().setSharedPreferencesMode(Context.MODE_WORLD_READABLE);
+//        addPreferencesFromResource(R.xml.settings);
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(android.R.id.content, new SettingsFragment())
+                    .commit();
+        }
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
-    }
+    public static class SettingsFragment extends PreferenceFragment implements
+            SharedPreferences.OnSharedPreferenceChangeListener {
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(
-                this);
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            getPreferenceManager().setSharedPreferencesMode(Context.MODE_WORLD_READABLE);
+            addPreferencesFromResource(R.xml.settings);
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            // Enforce mutual exclusion of all caps and all lower case preferences
+            if (key.contentEquals("prefAllCaps") && sharedPreferences.getBoolean("prefAllCaps", false)) {
+                if (sharedPreferences.getBoolean("prefNoCaps", false)) {
+                    SwitchPreferenceCompat pref = (SwitchPreferenceCompat) findPreference("prefNoCaps");
+                    pref.setChecked(false);
+                }
+            } else if (key.contentEquals("prefNoCaps")
+                    && sharedPreferences.getBoolean("prefNoCaps", false)) {
+                if (sharedPreferences.getBoolean("prefAllCaps", false)) {
+                    SwitchPreferenceCompat pref = (SwitchPreferenceCompat) findPreference("prefAllCaps");
+                    pref.setChecked(false);
+                }
+            }
+        }
     }
 
     @Override
@@ -58,23 +91,6 @@ public class AllCapsNoCapsPreferenceActivity extends PreferenceActivity implemen
         return false;
     }
 
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        // Enforce mutual exclusion of all caps and all lower case preferences
-        if (key.contentEquals("prefAllCaps") && sharedPreferences.getBoolean("prefAllCaps", false)) {
-            if (sharedPreferences.getBoolean("prefNoCaps", false)) {
-                SwitchPreference pref = (SwitchPreference) findPreference("prefNoCaps");
-                pref.setChecked(false);
-            }
-        } else if (key.contentEquals("prefNoCaps")
-                && sharedPreferences.getBoolean("prefNoCaps", false)) {
-            if (sharedPreferences.getBoolean("prefAllCaps", false)) {
-                SwitchPreference pref = (SwitchPreference) findPreference("prefAllCaps");
-                pref.setChecked(false);
-            }
-        }
-    }
-
     public static String getVersion(Context context) {
         String version = "1.0";
         try {
@@ -86,5 +102,4 @@ public class AllCapsNoCapsPreferenceActivity extends PreferenceActivity implemen
         }
         return version;
     }
-
 }
